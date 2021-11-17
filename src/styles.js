000a902768a1bbf0770b798601f630a2a5096fc7
김윤혁 <kyhyeok@gmail.com>
2021-11-18 08:39:24 +0900

    #10.14 Dark Mode

--- src/styles.js
+++ src/styles.js
@@ -3,11 +3,15 @@ import reset from "styled-reset";
 
 export const lightTheme = {
     accent: "#0095f6",
+    bgColor: "#fafafa",
+    fontColor: "rgb(38, 38, 38)",
     borderColor: "rgb(219, 219, 219)",
 };
 
 export const darkTheme = {
+    fontColor: "white",
     borderColor: "#0095f6",
+    bgColor: "#000",
     accent: "rgb(219, 219, 219)",
 };
 
@@ -20,10 +24,10 @@ export const GlobalStyles = createGlobalStyle`
         box-sizing: border-box
     }
     body {
-        background-color: #fafafa;
+        background-color: ${(props) => props.theme.bgColor};
         font-size: 14px;
         font-family: "Open Sans", sans-serif;
-        color: rgb(38, 38, 38)
+        color: ${(props) => props.theme.fontColor};
     }
     a {
         text-decoration: none;
