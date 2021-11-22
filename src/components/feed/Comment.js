e5cb244edb3ebccebc20854feb20e540bf93104a
김윤혁 <kyhyeok@gmail.com>
2021-11-22 12:58:18 +0900

    hashtags

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,4 +1,6 @@
+import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { FatText } from "../shared";
 
@@ -6,13 +8,34 @@ const CommentContainer = styled.div``;
 
 const CommentCaption = styled.span`
   margin-left: 10px;
+  a {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  a::after,
+  span::after {
+    content: " ";
+  }
 `;
 
 const Comment = ({ author, payload }) => {
+  const hashTagLink = payload.split(" ").map((word, index) =>
+    /#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/.test(word) ? (
+      <React.Fragment key={index}>
+        <Link to={`/hashtags/${word}`}>{word} </Link>
+      </React.Fragment>
+    ) : (
+      <React.Fragment key={index}>{word} </React.Fragment>
+    )
+  );
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption>{hashTagLink}</CommentCaption>
     </CommentContainer>
   );
 };
