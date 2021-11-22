c6e84f9cf07649dea879e319a3ee3b7ecdb4cf81
김윤혁 <kyhyeok@gmail.com>
2021-11-22 15:28:31 +0900

    #11.16 11.17 11.18 11.19 Comment

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { gql, useMutation } from "@apollo/client";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { FatText } from "../shared";
 
-const CommentContainer = styled.div``;
+const DELETE_COMMENT_MUTATION = gql`
+  mutation deleteComment($id: Int!) {
+    deleteComment(id: $id) {
+      ok
+    }
+  }
+`;
+
+const CommentContainer = styled.div`
+  margin-bottom: 8px;
+`;
 
 const CommentCaption = styled.span`
   margin-left: 10px;
@@ -22,7 +33,47 @@ const CommentCaption = styled.span`
   }
 `;
 
-const Comment = ({ author, payload }) => {
+const DeleteCommentButton = styled.button`
+  appearance: none;
+  margin-left: 5px;
+  background: #dc3545;
+  color: #fff;
+  text-align: center;
+  border: none;
+  border-radius: 4px;
+  box-shadow: 0 4px 6px -1px rgb(0 0 0 / 10%), 0 2px 4px -1px rgb(0 0 0 / 6%);
+  cursor: pointer;
+  &:hover {
+    background-color: #c82333;
+  }
+`;
+
+const Comment = ({ id, photoId, author, payload, isMine }) => {
+  const updateDeleteComment = (cache, result) => {
+    const {
+      data: {
+        deleteComment: { ok },
+      },
+    } = result;
+    if (ok) {
+      cache.evict({ id: `Comment:${id}` });
+      cache.modify({
+        id: `Photo:${photoId}`,
+        fields: {
+          commentNumber(prev) {
+            return prev - 1;
+          },
+        },
+      });
+    }
+  };
+  const [deleteCommentMutation] = useMutation(DELETE_COMMENT_MUTATION, {
+    variables: {
+      id,
+    },
+    update: updateDeleteComment,
+  });
+
   const hashTagLink = payload.split(" ").map((word, index) =>
     /#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/.test(word) ? (
       <React.Fragment key={index}>
@@ -32,17 +83,26 @@ const Comment = ({ author, payload }) => {
       <React.Fragment key={index}>{word} </React.Fragment>
     )
   );
+  const onDeleteClick = () => {
+    deleteCommentMutation();
+  };
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
       <CommentCaption>{hashTagLink}</CommentCaption>
+      {isMine ? (
+        <DeleteCommentButton onClick={onDeleteClick}>X</DeleteCommentButton>
+      ) : null}
     </CommentContainer>
   );
 };
 
 Comment.propTypes = {
+  id: PropTypes.number,
+  photoId: PropTypes.number,
   author: PropTypes.string.isRequired,
   payload: PropTypes.string.isRequired,
+  isMine: PropTypes.bool,
 };
 
 export default Comment;
--- src/components/feed/Comments.js
+++ src/components/feed/Comments.js
@@ -1,10 +1,24 @@
+import { gql, useMutation } from "@apollo/client";
 import PropTypes from "prop-types";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import Comment from "./Comment";
+import useUser from "../../hooks/useUser";
+
+const CREATE_COMMENT_MUTATION = gql`
+  mutation createComment($photoId: Int!, $payload: String!) {
+    createComment(photoId: $photoId, payload: $payload) {
+      ok
+      id
+      error
+    }
+  }
+`;
 
 const CommentsContainer = styled.div`
   margin-top: 20px;
 `;
+
 const CommentCount = styled.span`
   opacity: 0.7;
   margin: 10px 0px;
@@ -13,7 +27,90 @@ const CommentCount = styled.span`
   font-size: 10px;
 `;
 
-function Comments({ author, caption, commentNumber, comments }) {
+const PostCommentContainer = styled.div`
+  margin-top: 10px;
+  padding-top: 15px;
+  padding-bottom: 10px;
+  border-top: 1px solid ${(props) => props.theme.borderColor};
+`;
+
+const PostCommentInput = styled.input`
+  width: 100%;
+  &::placeholder {
+    font-size: 12px;
+  }
+`;
+
+function Comments({ photoId, author, caption, commentNumber, comments }) {
+  const { register, handleSubmit, setValue, getValues } = useForm();
+  const { data: userData } = useUser();
+  const createCommentUpdate = (cache, result) => {
+    const { payload } = getValues();
+    setValue("payload", "");
+    const {
+      data: {
+        createComment: { ok, id },
+      },
+    } = result;
+    if (ok && userData?.me) {
+      const newComment = {
+        __typename: "Comment",
+        createdAt: Date.now() + "",
+        id,
+        isMine: true,
+        payload,
+        user: {
+          ...userData.me,
+        },
+      };
+      const newCacheComment = cache.writeFragment({
+        data: newComment,
+        fragment: gql`
+          fragment BSName on Commnet {
+            id
+            payload
+            isMine
+            user {
+              username
+              avatar
+            }
+            createdAt
+          }
+        `,
+      });
+
+      cache.modify({
+        id: `Photo:${photoId}`,
+        fields: {
+          comments(prev) {
+            return [...prev, newCacheComment];
+          },
+          commentNumber(prev) {
+            return prev + 1;
+          },
+        },
+      });
+    }
+  };
+
+  const [createCommentMutation, { loading }] = useMutation(
+    CREATE_COMMENT_MUTATION,
+    {
+      update: createCommentUpdate,
+    }
+  );
+  const onValid = (data) => {
+    const { payload } = data;
+    if (loading) {
+      return;
+    }
+    createCommentMutation({
+      variables: {
+        photoId,
+        payload,
+      },
+    });
+  };
   return (
     <CommentsContainer>
       <Comment author={author} payload={caption} />
@@ -23,15 +120,29 @@ function Comments({ author, caption, commentNumber, comments }) {
       {comments?.map((comment) => (
         <Comment
           key={comment.id}
+          id={comment.id}
+          photoId={photoId}
           author={comment.user.username}
           payload={comment.payload}
+          isMine={comment.isMine}
         />
       ))}
+      <PostCommentContainer>
+        <form onSubmit={handleSubmit(onValid)}>
+          <PostCommentInput
+            name="payload"
+            ref={register({ required: true })}
+            type="text"
+            placeholder="댓글을 입력해주세요."
+          />
+        </form>
+      </PostCommentContainer>
     </CommentsContainer>
   );
 }
 
 Comments.propTypes = {
+  photoId: PropTypes.number.isRequired,
   author: PropTypes.string.isRequired,
   caption: PropTypes.string,
   commentNumber: PropTypes.number.isRequired,
