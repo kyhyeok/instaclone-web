b31d6bf63de88aa094e40d33e8ec43686ff15640
김윤혁 <kyhyeok@gmail.com>
2021-11-23 10:05:27 +0900

    #12.3 Follow Unfollow Part One

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,6 +1,10 @@
 import { useParams } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
+import { faHeart, faComment } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { PHOTO_FRAGMENT } from "../components/fragments";
+import { FatText } from "../components/shared";
+import styled from "styled-components";
 
 const SEE_PROFILE_QUERY = gql`
   query seeProfile($username: String!) {
@@ -22,6 +26,87 @@ const SEE_PROFILE_QUERY = gql`
   ${PHOTO_FRAGMENT}
 `;
 
+const Header = styled.div`
+  display: flex;
+`;
+
+const Avatar = styled.img`
+  margin-left: 50px;
+  height: 160px;
+  width: 160px;
+  border-radius: 50%;
+  margin-right: 150px;
+  background-color: #2c2c2c;
+`;
+
+const Column = styled.div``;
+
+const Row = styled.div`
+  margin-bottom: 20px;
+  font-size: 16px;
+`;
+
+const Username = styled.h3`
+  font-size: 28px;
+  font-weight: 400;
+`;
+
+const List = styled.ul`
+  display: flex;
+`;
+
+const Item = styled.li`
+  margin-right: 20px;
+`;
+
+const Value = styled(FatText)`
+  font-size: 18px;
+`;
+
+const Name = styled(FatText)`
+  font-size: 20px;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-auto-rows: 290px;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 30px;
+  margin-top: 50px;
+`;
+
+const Photo = styled.div`
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  position: relative;
+`;
+
+const Icons = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  opacity: 0;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const Icon = styled.span`
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  margin: 0px 5px;
+  svg {
+    font-size: 14px;
+    margin-right: 5px;
+  }
+`;
+
 const Profile = () => {
   const { username } = useParams();
   const { data } = useQuery(SEE_PROFILE_QUERY, {
@@ -29,8 +114,54 @@ const Profile = () => {
       username,
     },
   });
-  console.log(data);
-  return "Profile";
+  return (
+    <div>
+      <Header>
+        <Avatar src={data?.seeProfile?.avatar} />
+        <Column>
+          <Row>
+            <Username>{data?.seeProfile?.username}</Username>
+          </Row>
+          <Row>
+            <List>
+              <Item>
+                <span>
+                  <Value>{data?.seeProfile?.totalFollowers}</Value> followers
+                </span>
+              </Item>
+              <Item>
+                <span>
+                  <Value>{data?.seeProfile?.totalFollowing}</Value> floowing
+                </span>
+              </Item>
+            </List>
+          </Row>
+          <Row>
+            <Name>
+              {data?.seeProfile?.firstName} {data?.seeProfile?.lastName}
+            </Name>
+          </Row>
+          <Row>{data?.seeProfile?.bio}</Row>
+        </Column>
+      </Header>
+      <Grid>
+        {data?.seeProfile?.photos.map((photo) => (
+          <Photo bg={photo.file}>
+            <Icons>
+              <Icon>
+                <FontAwesomeIcon icon={faHeart} />
+                {photo.likes}
+              </Icon>
+              <Icon>
+                <FontAwesomeIcon icon={faComment} />
+                {photo.commentNumber}
+              </Icon>
+            </Icons>
+          </Photo>
+        ))}
+      </Grid>
+    </div>
+  );
 };
 
 export default Profile;
